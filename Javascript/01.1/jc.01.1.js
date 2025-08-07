c8d84f4db2a315f8db1f48b40711b6d7d424c6b7
DSM2 <fatec-dsm2@academico.intra>
2025-08-07 19:20:46 -0300

    @vgmandira7

--- Javascript/01.1/jc.01.1.js
+++ Javascript/01.1/jc.01.1.js
@@ -1,10 +1,28 @@
+//Variaveis
+
+//var (pode mudar a qualquer momento)
+//let (Pode reatribuido, mas não redeclarado)
+//const (não muda)
+
 // TIPOS DE FUNÇÕES
 
-
 //FUNÇÃO SIMPLES
+const message = "<h2>Olá, seja bem vindo. Essa é sua primeira função!</h2>";
 function showMessage() {
-    document.write("<h2>Olá, seja bem vindo. Essa é sua primeira função!</h2>");
+  document.write(message);
 }
 
 // invocando a função
 showMessage();
+
+// Função com parametro
+const user = "Vitor Gabriel";
+
+function userMessage(user) { //esse user é parametro, não precisa ter o mesmo nome da variavel
+  document.write(`<h3> o que deseja fazer hoje, ${user} ? </h3>`);
+  //${} = template strings ou literal strings
+  //É usado para inserir variaveis dentro de string, no caso aspas
+}
+
+userMessage(user); // Esse user é um argumento, uma coisa que estou enviando para funçaõ, por isso precisa 
+//ter o mesmo nome da variavel    
