d1a3cf1ae4f0da3f9751ad9f723c505d28df58ed
DSM2 <fatec-dsm2@academico.intra>
2025-08-06 21:25:40 -0300

    @vgmandira7

---
+++ Javascript/01.1/jc.01.1.js
@@ -0,0 +1,10 @@
+// TIPOS DE FUNÇÕES
+
+
+//FUNÇÃO SIMPLES
+function showMessage() {
+    document.write("<h2>Olá, seja bem vindo. Essa é sua primeira função!</h2>");
+}
+
+// invocando a função
+showMessage();
